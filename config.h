4be4864d23d82732fbafb1b08b73736c1e567cb7
FlyingBBQ <derek.cardol@gmail.com>
2020-12-23 21:48:46 +0100

    Change colors

--- config.h
+++ config.h
@@ -118,30 +118,30 @@ static const char *colorname[] = {
 	"white",
 #endif
 
-    [0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-    [1] = "#cc241d", /* red     */
-    [2] = "#98971a", /* green   */
-    [3] = "#d79921", /* yellow  */
-    [4] = "#458588", /* blue    */
-    [5] = "#b16286", /* magenta */
-    [6] = "#689d6a", /* cyan    */
-    [7] = "#a89984", /* white   */
+    [0] = "#2b2b2b", /* background */
+    [1] = "#bc3f3c", /* red     */
+    [2] = "#6a8759", /* green   */
+    [3] = "#bbb529", /* yellow  */
+    [4] = "#6897bb", /* blue    */
+    [5] = "#9876aa", /* magenta */
+    [6] = "#b5e6a3", /* cyan    */
+    [7] = "#a9b7c6", /* white   */
 
     /* 8 bright colors */
-    [8]  = "#928374", /* black   */
-    [9]  = "#fb4934", /* red     */
-    [10] = "#b8bb26", /* green   */
-    [11] = "#fabd2f", /* yellow  */
-    [12] = "#83a598", /* blue    */
-    [13] = "#d3869b", /* magenta */
-    [14] = "#8ec07c", /* cyan    */
-    [15] = "#ebdbb2", /* white   */
+    [8]  = "#2b2b2b", /* black   */
+    [9]  = "#cc7832", /* red     */
+    [10] = "#6a8759", /* green   */
+    [11] = "#ffc66d", /* yellow  */
+    [12] = "#6897bb", /* blue    */
+    [13] = "#9876aa", /* magenta */
+    [14] = "#a8c023", /* cyan    */
+    [15] = "#bbbbbb", /* white   */
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-    "#ebdbb2",
-    "#282828",
+    "#bbbbbb",
+    "#2b2b2b",
 };
 
 
